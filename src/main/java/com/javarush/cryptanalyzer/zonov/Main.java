752d429e1bb810d95a919aff625181c9c2bbe38c
Dmitriy Zonov <beeline343@gmail.com>
2023-05-05 11:12:49 +0200

    Added input text

--- src/main/java/com/javarush/cryptanalyzer/zonov/Main.java
+++ src/main/java/com/javarush/cryptanalyzer/zonov/Main.java
@@ -1,10 +1,9 @@
 package com.javarush.cryptanalyzer.zonov;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
     }
 }
