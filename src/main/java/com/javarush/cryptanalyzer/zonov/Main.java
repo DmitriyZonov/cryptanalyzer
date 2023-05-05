e90ed46dec95809bc7737b0535d7dfdb70f64fd4
Dmitriy Zonov <beeline343@gmail.com>
2023-05-05 16:03:57 +0200

    fix Text Expressions and Main (console out)

--- src/main/java/com/javarush/cryptanalyzer/zonov/Main.java
+++ src/main/java/com/javarush/cryptanalyzer/zonov/Main.java
@@ -8,42 +8,41 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws IOException, NoSuchElementException {
         System.out.println(TextExpressions.GREETING);
-        Scanner console = new Scanner(System.in);
-        System.out.println(TextExpressions.MAIN_CHOICE);
-        boolean rightMainChoice = false;
-        while(!(rightMainChoice)) {
-            int mainAnswer = console.nextInt();
-            if (mainAnswer == 0) {
-                rightMainChoice = true;
-                System.out.println(TextExpressions.CHOICE_ENCRYPTION);
-                boolean rightChoiceOfEncryptionMode = false;
-                while(!(rightChoiceOfEncryptionMode)) {
-                    int answer = console.nextInt();
-                    if (answer == 0) {
-                        rightChoiceOfEncryptionMode = true;
-                        Encryption.setKey();
-                        Encryption.encrypt();
-                    } else if (answer == 1) {
-                        rightChoiceOfEncryptionMode = true;
-                        System.out.println(TextExpressions.GET_INPUT_FILE_PATH);
-                        Encryption.setInputFileName(console.next());
-                        System.out.println(TextExpressions.GET_OUTPUT_FILE_PATH);
-                        Encryption.setOutputFileName(console.next());
-                        Encryption.setKey();
-                        Encryption.encrypt();
-                        console.close();
-                    } else {
-                        System.out.println(TextExpressions.WRONG_CHOICE);
+        try (Scanner console = new Scanner(System.in)) {
+            System.out.println(TextExpressions.MAIN_CHOICE);
+            boolean rightMainChoice = false;
+            while (!(rightMainChoice)) {
+                int mainAnswer = console.nextInt();
+                if (mainAnswer == 0) {
+                    rightMainChoice = true;
+                    System.out.println(TextExpressions.CHOICE_ENCRYPTION);
+                    boolean rightChoiceOfEncryptionMode = false;
+                    while (!(rightChoiceOfEncryptionMode)) {
+                        int answer = console.nextInt();
+                        if (answer == 0) {
+                            rightChoiceOfEncryptionMode = true;
+                            Encryption.setKey();
+                            Encryption.encrypt();
+                        } else if (answer == 1) {
+                            rightChoiceOfEncryptionMode = true;
+                            System.out.println(TextExpressions.GET_INPUT_FILE_PATH);
+                            Encryption.setInputFileName(console.next());
+                            System.out.println(TextExpressions.GET_OUTPUT_FILE_PATH);
+                            Encryption.setOutputFileName(console.next());
+                            Encryption.setKey();
+                            Encryption.encrypt();
+                            console.close();
+                        } else {
+                            System.out.println(TextExpressions.WRONG_CHOICE);
+                        }
                     }
-            }
-        } else if (mainAnswer == 1) {
-                rightMainChoice = true;
-            } else {
-                System.out.println(TextExpressions.WRONG_CHOICE);
-            }
+                } else if (mainAnswer == 1) {
+                    rightMainChoice = true;
 
-
+                } else {
+                    System.out.println(TextExpressions.WRONG_MAIN_CHOICE);
+                }
+            }
         }
-        console.close();
     }
 }
--- src/main/java/com/javarush/cryptanalyzer/zonov/TextExpressions.java
+++ src/main/java/com/javarush/cryptanalyzer/zonov/TextExpressions.java
@@ -3,6 +3,7 @@ package com.javarush.cryptanalyzer.zonov;
 public class TextExpressions {
     public static final String GREETING = "Добро пожаловать в программу по шифрованию текста \"Цезарь\"" + "\n";
     public static final String MAIN_CHOICE = "Выберете режим работы:\n" + "Нажмите 0 для шифрования файла;\n" + "Нажмите 1 для расшифровки файла;\n";
+    public static final String WRONG_MAIN_CHOICE = "Выбрано неверное число, пожалуйста, введите следующие числа: \n" + "\n" + MAIN_CHOICE;
     public static final String CHOICE_ENCRYPTION = "Нажмите 0 для выбора тестового файла для шифрования;\n" +
                                         "Нажмите 1 для указания собственных путей файлов для шифрования и получения результата;\n";
     public static final String WRONG_CHOICE = "Выбрано неверное число, пожалуйста, введите следующие числа: \n" + "\n" + CHOICE_ENCRYPTION;
